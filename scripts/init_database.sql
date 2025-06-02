938c98f163402e5a0e949d45eea34d782331fc9f
robindude8000 <arjay.ocampo16@gmail.com>
2025-06-02 08:24:57 +0800

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -106,8 +106,79 @@ CREATE TABLE bronze.erp_px_cat_g1v2 (
 );
 
 
+/*
+===== Load Data into Tables =====
+Script Purpose:
+	To first delete the existing data (if not empty) in the table 
+	then load all data into bronze.crm and bronze.erp tables from specific source (file path).
+*/
 
+TRUNCATE TABLE bronze.crm_cust_info;
 
+BULK INSERT bronze.crm_cust_info
+FROM 'C:\Users\Robindude\Documents\0. SQL\Baraa\Data Warehousing\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK 
+);
+
+TRUNCATE TABLE bronze.crm_prod_info;
+
+BULK INSERT bronze.crm_prod_info
+FROM 'C:\Users\Robindude\Documents\0. SQL\Baraa\Data Warehousing\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK 
+);
+
+TRUNCATE TABLE bronze.crm_sales_details;
+
+BULK INSERT bronze.crm_sales_details
+FROM 'C:\Users\Robindude\Documents\0. SQL\Baraa\Data Warehousing\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK 
+);
+
+TRUNCATE TABLE bronze.erp_cust_az12;
+
+BULK INSERT bronze.erp_cust_az12
+FROM 'C:\Users\Robindude\Documents\0. SQL\Baraa\Data Warehousing\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK 
+);
+
+TRUNCATE TABLE bronze.erp_loc_a101;
+
+BULK INSERT bronze.erp_loc_a101
+FROM 'C:\Users\Robindude\Documents\0. SQL\Baraa\Data Warehousing\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK 
+);
+
+TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+BULK INSERT bronze.erp_px_cat_g1v2
+FROM 'C:\Users\Robindude\Documents\0. SQL\Baraa\Data Warehousing\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK 
+);
+
+SELECT * FROM bronze.crm_cust_info;
+SELECT * FROM bronze.crm_prod_info;
+SELECT * FROM bronze.crm_sales_details;
+SELECT * FROM bronze.erp_cust_az12;
+SELECT * FROM bronze.erp_loc_a101;
+SELECT * FROM bronze.erp_px_cat_g1v2;
 
 
 
